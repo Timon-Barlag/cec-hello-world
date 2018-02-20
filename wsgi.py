1d41545b0d58d1684d9aa12a88a42321c78be808
Timon Barlag <timon.barlag@gmx.de>
2018-02-20 13:03:33 +0200

    Workiking version

--- wsgi.py
+++ wsgi.py
@@ -8,42 +8,45 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    WRITE_TO_LOG = True
+    WRITE_TO_LOG = True	      #specify whether or not to write to log file
 
-    if socket.gethostname().startswith("cec-hello-world"):
+    hostname = socket.gethostname()
+
+    #specify write path, depending on whether on cec-hello-world or not
+    if hostname.startswith("cec-hello-world"):
         write_path = "/mnt/"
     else:
         write_path = "/home/t/Desktop/tmp/tmp2/"
 
-
-    hostname = socket.gethostname()
-
     hello_string = "Hello World! Greetings from " + hostname + "\n"
-    #hello_string += "/mnt/ content: " + str(os.listdir("mnt")) + "\n"
+
+
+    #write content of write_path directory if it exists and list contents of current directory otherwise 
     if os.path.exists(write_path):
         hello_string += "<p>" + write_path + " content: " + str(os.listdir(write_path)) + "</p>\n"
     else:
         hello_string += "<p>" + write_path + " not foudn</p>\n"
         hello_string += str(next(os.walk('.'))[1])
 
-    #hello_string += "_________"
-
     timestamp = time.time()
 
+
+    #write to log if specified
     if WRITE_TO_LOG:
         with open(write_path + "my_log.log", "a") as file:
             file.write("host: " + hostname + "  time: " + str(timestamp) + "\n")
 
+    #read from log if the file exists
     if os.path.exists(write_path + "my_log.log"):
         content = ""
         with open(write_path + "my_log.log") as f:
             for line in f:
-                #print("test")
                 content += line.replace("\n", "") + "<br>\n"	
-            #content = f.read()
 
         hello_string += "<p>content:</p>\n" + content + "<p>end content</p>\n" 
-
+    else:
+    	hello_string += "<p>logfile not found</p>\n"
+    	
     return hello_string
 
 
